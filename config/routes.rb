f40e734b61e9fab2589a448a006078fb5552d41e
tbanish <tombanish@gmail.com>
2021-03-21 17:05:52 -0400

    add destroy route for cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :decks, only: [:index, :show, :create, :update, :destroy]
-      resources :cards, only: [:index, :show, :create, :update]
+      resources :cards, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
