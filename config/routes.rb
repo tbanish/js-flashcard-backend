16d2f303b3bb875b220ce3c07e56e35162f3a6dd
tbanish <tombanish@gmail.com>
2021-03-21 16:41:23 -0400

    add show route for cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :decks, only: [:index, :show]
-      resources :cards, only: [:index]
+      resources :cards, only: [:index, :show]
     end
   end
 end
