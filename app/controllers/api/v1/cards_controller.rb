7ce1351037568ce2f0391cf549d1c5ce0ac2cba4
tbanish <tombanish@gmail.com>
2021-03-21 16:35:49 -0400

    create private method card_params

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -3,4 +3,10 @@ class Api::V1::CardsController < ApplicationController
     cards = Card.all
     render json: CardSerializer.new(cards)
   end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:question, :answer, :deck_id)
+  end
 end
