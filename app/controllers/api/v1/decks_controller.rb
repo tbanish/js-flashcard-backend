8b4c904d8fc5dd6209772f88f0af6776b5be4b38
tbanish <tombanish@gmail.com>
2021-03-21 16:51:42 -0400

    build create action in decks controller

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -9,6 +9,15 @@ class Api::V1::DecksController < ApplicationController
     render json: DeckSerializer.new(deck)
   end
 
+  def create
+    deck = Deck.new(deck_params)
+    if deck.save
+      render json: deck, status: :accepted
+    else
+      render json: { errors: deck.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   private
 
   def deck_params
