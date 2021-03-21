8831e851eab3babcb1096bf0de092098740a9624
tbanish <tombanish@gmail.com>
2021-03-21 16:39:24 -0400

    add show route for decks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :decks, only: [:index]
+      resources :decks, only: [:index, :show]
       resources :cards, only: [:index]
     end
   end
