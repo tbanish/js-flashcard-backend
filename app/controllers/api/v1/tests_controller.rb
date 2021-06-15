7aa4b04e2585b70031c4724c35ff43805ff10d19
tbanish <tombanish@gmail.com>
2021-06-15 19:56:10 -0400

    build create action and strong_params in test controller

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::TestsController < ApplicationController
     tests = Test.all
     render json: TestSerializer.new(tests)
   end
+
+  def create
+    test = Test.new(test_params)
+
+    if test.save
+      render json: test, status: :accepted
+    else
+      render json: { errors: test.errors.full_messages }
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:duration, :score, :correct_ids, :incorrect_ids, :deck_id)
+  end
 end
